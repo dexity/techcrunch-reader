5a041e48661af4f991e6f1ee5146f033fc2bfe50
Alex Dementsov <dexity@gmail.com>
2016-12-18 19:29:21 -0800

    Converted tabs to spaces

--- src/index.js
+++ src/index.js
@@ -16,20 +16,20 @@ var request = require('request');
 var https = require('https');
 
 var HELP_TEXT = 'TechCrunch helps you to read online articles. ' + 
-	'For example, you can ask thinkgs like: read articles from Startup category. ' +
-	'Or, list categories. What would you like to read today?';
+    'For example, you can ask thinkgs like: read articles from Startup category. ' +
+    'Or, list categories. What would you like to read today?';
 var ERROR_TEXT = 'Ooops. Something went wrong.';
 var SERVICE_ERROR_TEXT = 'Something is wrong with TechCrunch';
 var REPROMPT_TEXT = 'What would you like to read?';
 var BASE_URL = 'https://techcrunch.com/';
 var CATEGORIES = {
-	news: '',
-	startups: 'startups',
-	mobile: 'mobile',
-	gadgets: 'gadgets',
-	enterprise: 'enterprise',
-	social: 'social',
-	europe: 'europe'
+    news: '',
+    startups: 'startups',
+    mobile: 'mobile',
+    gadgets: 'gadgets',
+    enterprise: 'enterprise',
+    social: 'social',
+    europe: 'europe'
 }
 
 
@@ -43,244 +43,244 @@ TechCrunchReader.prototype = Object.create(AlexaSkill.prototype);
 TechCrunchReader.prototype.constructor = TechCrunchReader;
 
 TechCrunchReader.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
-	console.log('Session started');
-	initSession(session);
+    console.log('Session started');
+    initSession(session);
 }
 
 
 TechCrunchReader.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-	handleAskResponse(response, HELP_TEXT, REPROMPT_TEXT);
+    handleAskResponse(response, HELP_TEXT, REPROMPT_TEXT);
 }
 
 
 TechCrunchReader.prototype.intentHandlers = {
-	ListCategoryIntent: function(intent, session, response) {
-		listCategoryIntent(intent, session, response);
-	},
-	ListByCategoryIntent: function(intent, session, response) {
-		listByCategoryIntent(intent, session, response);
-	},
-  	ArticleIntent: function(intent, session, response) {
-  		articleIntent(intent, session, response);
-  	},
-  	RepeatArticleIntent: function(intent, session, response) {
-  		repeatArticleIntent(intent, session, response);
-  	},
-  	'AMAZON.HelpIntent': function(intent, session, response) {
-  		helpIntent(intent, session, response);
-  	},
-  	'AMAZON.StopIntent': function(intent, session, response) {
-  		stopIntent(intent, session, response);
-  	},
-  	'AMAZON.CancelIntent': function(intent, session, response) {
-  		cancelIntent(intent, session, response);
-  	}
+    ListCategoryIntent: function(intent, session, response) {
+        listCategoryIntent(intent, session, response);
+    },
+    ListByCategoryIntent: function(intent, session, response) {
+        listByCategoryIntent(intent, session, response);
+    },
+    ArticleIntent: function(intent, session, response) {
+        articleIntent(intent, session, response);
+    },
+    RepeatArticleIntent: function(intent, session, response) {
+        repeatArticleIntent(intent, session, response);
+    },
+    'AMAZON.HelpIntent': function(intent, session, response) {
+        helpIntent(intent, session, response);
+    },
+    'AMAZON.StopIntent': function(intent, session, response) {
+        stopIntent(intent, session, response);
+    },
+    'AMAZON.CancelIntent': function(intent, session, response) {
+        cancelIntent(intent, session, response);
+    }
 }
 
 // Intents
 
 var listCategoryIntent = function(intent, session, response) {
-	// Lists categories
-	var speechText = 'Category options are ' + getCategoriesText();
-	speechText += 'What category would you like to read news from?'
-	handleAskResponse(response, speechText, REPROMPT_TEXT);
+    // Lists categories
+    var speechText = 'Category options are ' + getCategoriesText();
+    speechText += 'What category would you like to read news from?'
+    handleAskResponse(response, speechText, REPROMPT_TEXT);
 }
 
 var listByCategoryIntent = function(intent, session, response) {
-	// List articles by category
-	var category = cleanCategory(intent.slots.Category);
-	var repromptQuestion = 'Category options are ' + getCategoriesText();
-	if (!category) {
-		handleErrorResponse(response, repromptQuestion);
-	}
-	console.log('Category: ' + category);
-	var url = BASE_URL + CATEGORIES[category];
-	console.log('Request url: ' + url);
+    // List articles by category
+    var category = cleanCategory(intent.slots.Category);
+    var repromptQuestion = 'Category options are ' + getCategoriesText();
+    if (!category) {
+        handleErrorResponse(response, repromptQuestion);
+    }
+    console.log('Category: ' + category);
+    var url = BASE_URL + CATEGORIES[category];
+    console.log('Request url: ' + url);
 
-	requestListArticles(url, response, function(data){
+    requestListArticles(url, response, function(data){
         // Response format: 1. Title A. 2. Title B. ...
         var speechText = '';
         if (data.length) {
-			// Set current category and list of urls for the category
-			session.attributes.currentCategory = category;
-			session.attributes.categoryUrls[category] = [];
-			for (var i = 0; i < data.length; i++){
-				var order = i + 1;
-	        	// Set article urls
-	        	session.attributes.categoryUrls[category].push(data[i][1]);
-	            speechText += order + '. ' + data[i][0] + '. ';		
-			}
-	        handleTellResponse(response, speechText);
-	    } else { 
-        	speechText = 'There are no articles for ' + category + ' category.';
-        	handleErrorResponse(response, speechText);
+            // Set current category and list of urls for the category
+            session.attributes.currentCategory = category;
+            session.attributes.categoryUrls[category] = [];
+            for (var i = 0; i < data.length; i++){
+                var order = i + 1;
+                // Set article urls
+                session.attributes.categoryUrls[category].push(data[i][1]);
+                speechText += order + '. ' + data[i][0] + '. ';     
+            }
+            handleTellResponse(response, speechText);
+        } else { 
+            speechText = 'There are no articles for ' + category + ' category.';
+            handleErrorResponse(response, speechText);
         }
-	});
+    });
 }
 
 var articleIntent = function(intent, session, response) {
-	// Reads article
-	var category = cleanCategory(intent.slots.Category);
-	var order = intent.slots.Order.value;
-	var speechText = '';
-	var url;
-	var url2;
+    // Reads article
+    var category = cleanCategory(intent.slots.Category);
+    var order = intent.slots.Order.value;
+    var speechText = '';
+    var url;
+    var url2;
 
-	// Set default category
-	if (!category) {
-		// If current category is set, use it
-		if (session.attributes.currentCategory) {
-			category = session.attributes.currentCategory;
-		} else {
-			category = 'news';  // Default category
-		}
-	}
-	if (!session.attributes.categoryUrls[category]) {
-		session.attributes.categoryUrls[category] = [];
-	}
+    // Set default category
+    if (!category) {
+        // If current category is set, use it
+        if (session.attributes.currentCategory) {
+            category = session.attributes.currentCategory;
+        } else {
+            category = 'news';  // Default category
+        }
+    }
+    if (!session.attributes.categoryUrls[category]) {
+        session.attributes.categoryUrls[category] = [];
+    }
 
-	var isValidOrder = function(order_) {
-		return (order_ !== undefined && order_ > 0 &&
-				order_ <= session.attributes.categoryUrls[category].length);
-	}
+    var isValidOrder = function(order_) {
+        return (order_ !== undefined && order_ > 0 &&
+                order_ <= session.attributes.categoryUrls[category].length);
+    }
 
-	if (isValidOrder(order)) {
-		url = session.attributes.categoryUrls[category][order-1];
-		requestArticle(url, response, function(title, author, article) {
-			session.attributes.currentCategory = category;
-			session.attributes.currentOrder = order;
-			session.attributes.currentUrl = url;
-	        articleResponse(response, title, author, article);
-		});	
-	} else {  // Order is not set or invalid
-		url = BASE_URL + CATEGORIES[category];
-		// Latest article from news: get list of articles, then get article details
-		requestListArticles(url, response, function(data){
-			if (data.length) {
-				for (var i = 0; i < data.length; i++){
-					session.attributes.categoryUrls[category].push(data[i][1]);
-				}
-				var index = 0;
-				if (isValidOrder(order)) {
-					index = order - 1;
-				}
-				url2 = data[index][1];  // Url of the first article
-				requestArticle(url2, response, function(title, author, article) {
-					session.attributes.currentCategory = category;
-					session.attributes.currentOrder = 1;
-					session.attributes.currentUrl = url2;
-			        articleResponse(response, title, author, article);
-				});
-			} else {
-				// Highly unlikely scenario
-				speechText = 'There are no articles to read.';
-        		handleErrorResponse(response, speechText);
-			} 
-		});
-	}
+    if (isValidOrder(order)) {
+        url = session.attributes.categoryUrls[category][order-1];
+        requestArticle(url, response, function(title, author, article) {
+            session.attributes.currentCategory = category;
+            session.attributes.currentOrder = order;
+            session.attributes.currentUrl = url;
+            articleResponse(response, title, author, article);
+        }); 
+    } else {  // Order is not set or invalid
+        url = BASE_URL + CATEGORIES[category];
+        // Latest article from news: get list of articles, then get article details
+        requestListArticles(url, response, function(data){
+            if (data.length) {
+                for (var i = 0; i < data.length; i++){
+                    session.attributes.categoryUrls[category].push(data[i][1]);
+                }
+                var index = 0;
+                if (isValidOrder(order)) {
+                    index = order - 1;
+                }
+                url2 = data[index][1];  // Url of the first article
+                requestArticle(url2, response, function(title, author, article) {
+                    session.attributes.currentCategory = category;
+                    session.attributes.currentOrder = 1;
+                    session.attributes.currentUrl = url2;
+                    articleResponse(response, title, author, article);
+                });
+            } else {
+                // Highly unlikely scenario
+                speechText = 'There are no articles to read.';
+                handleErrorResponse(response, speechText);
+            } 
+        });
+    }
 }
 
 
 var repeatArticleIntent = function(intent, session, response) {
-	// Reads recent article
-	if (session.attributes.currentUrl) {
-		requestArticle(session.attributes.currentUrl, response, 
-					   function(title, author, article) {
-	        articleResponse(response, title, author, article);
-		});
-	} else {
-		var speechText = 'There is no recent article.';
-		handleErrorResponse(response, speechText);
-	}
+    // Reads recent article
+    if (session.attributes.currentUrl) {
+        requestArticle(session.attributes.currentUrl, response, 
+                       function(title, author, article) {
+            articleResponse(response, title, author, article);
+        });
+    } else {
+        var speechText = 'There is no recent article.';
+        handleErrorResponse(response, speechText);
+    }
 }
 
 
 var helpIntent = function (intent, session, response) {
-	handleAskResponse(response, HELP_TEXT, REPROMPT_TEXT);
+    handleAskResponse(response, HELP_TEXT, REPROMPT_TEXT);
 }
 
 
 var stopIntent = function(intent, session, response) {
-	handleTellResponse(response, 'Goodbye', true);
+    handleTellResponse(response, 'Goodbye', true);
 }
 
 
 var cancelIntent = function(intent, session, response) {
-	handleTellResponse(response, 'Goodbye', true);
+    handleTellResponse(response, 'Goodbye', true);
 }
 
 // Utils functions
 
 var getCategoriesText = function() {
-	// Converts list of categories to string
-	return Object.keys(CATEGORIES).join(', ') + '. ';
+    // Converts list of categories to string
+    return Object.keys(CATEGORIES).join(', ') + '. ';
 }
 
 
 var cleanCategory = function(category) {
-	// Cleans category
-	if (!category.value) {
-		return
-	}
-	category = category.value.toLowerCase();
-	if (category.indexOf('start') === 0) {
-		category = 'startups';
-	}
-	if (category in CATEGORIES) {
-		return category;
-	}
+    // Cleans category
+    if (!category.value) {
+        return
+    }
+    category = category.value.toLowerCase();
+    if (category.indexOf('start') === 0) {
+        category = 'startups';
+    }
+    if (category in CATEGORIES) {
+        return category;
+    }
 }
 
 
 var cleanText = function(text) {
-	return text.replace(/[^\w\s\.]/gi, ' ').replace(/[\n\t]+/gi, ' ');
+    return text.replace(/[^\w\s\.]/gi, ' ').replace(/[\n\t]+/gi, ' ');
 }
 
 
 var requestListArticles = function(url, response, callback) {
-	// Makes request for list of articles by category
-	request(url, function(error, reqresp, html){
-	    if (error) {
-	    	handleErrorResponse(response, SERVICE_ERROR_TEXT)
-	    }
+    // Makes request for list of articles by category
+    request(url, function(error, reqresp, html){
+        if (error) {
+            handleErrorResponse(response, SERVICE_ERROR_TEXT)
+        }
         var $ = cheerio.load(html);
         var data = [];
         $('.post-title').filter(function() {
-        	var titleText = $(this).text();
-        	var articleUrl = $(this).find('a').attr('href');
-        	data.push([titleText, articleUrl]);
+            var titleText = $(this).text();
+            var articleUrl = $(this).find('a').attr('href');
+            data.push([titleText, articleUrl]);
         });
         callback(data)
-	})
+    })
 }
 
 
 var requestArticle = function(url, response, callback) {
-	// Requests article
-	request(url, function(error, reqresp, html){
-	    if (error){
-	    	handleErrorResponse(response, SERVICE_ERROR_TEXT)
-	    }
+    // Requests article
+    request(url, function(error, reqresp, html){
+        if (error){
+            handleErrorResponse(response, SERVICE_ERROR_TEXT)
+        }
         var $ = cheerio.load(html);
         var title = $('.tweet-title').text();
         var author = $('a[rel=author]').text();
         var article = $('.article-entry').text();
         article = cleanText(article)
-	    callback(title, author, article);
-	})	
+        callback(title, author, article);
+    })  
 }
 
 
 var articleResponse = function(response, title, author, article) {
-	// Response for article request
+    // Response for article request
     var speechText = title + '. by ' + author + '. ' + article;
     handleTellResponse(response, speechText);
 }
 
 
 var handleResponse = function(response, speechError, speechResponse, speechQuestion,
-							  repromptQuestion, shouldEndSession) {
-	// Handles Alexa response
+                              repromptQuestion, shouldEndSession) {
+    // Handles Alexa response
     var speechOutput;
     var repromptOutput;
     var cardTitle = 'TechCrunch Reader';
@@ -292,12 +292,12 @@ var handleResponse = function(response, speechError, speechResponse, speechQuest
         };
         response.tellWithCard(speechOutput, cardTitle, speechResponse, shouldEndSession);
     } else {
-    	var speechText = speechError;
-    	var repromptSpeechText = 'What would you like to read?';
-    	if (!speechError) {
-    		speechText = speechQuestion;
-    		repromptSpeechText = repromptQuestion;
-    	}
+        var speechText = speechError;
+        var repromptSpeechText = 'What would you like to read?';
+        if (!speechError) {
+            speechText = speechQuestion;
+            repromptSpeechText = repromptQuestion;
+        }
         speechOutput = {
             speech: speechText,
             type: AlexaSkill.speechOutputType.PLAIN_TEXT
@@ -313,28 +313,28 @@ var handleResponse = function(response, speechError, speechResponse, speechQuest
 
 
 var handleTellResponse = function(response, speechResponse, shouldEndSession) {
-	// Doesn't end session by default
-	handleResponse(response, null, speechResponse, null, null, shouldEndSession);
+    // Doesn't end session by default
+    handleResponse(response, null, speechResponse, null, null, shouldEndSession);
 }
 
 
 var handleErrorResponse = function(response, speechError) {
-	handleResponse(response, speechError);
+    handleResponse(response, speechError);
 }
 
 
 var handleAskResponse = function(response, speechQuestion, repromptQuestion) {
-	handleResponse(response, null, null, speechQuestion, repromptQuestion);
+    handleResponse(response, null, null, speechQuestion, repromptQuestion);
 }
 
 
 var initSession = function(session) {
-	session.attributes = {
-		currentCategory: '',
-		currentOrder: 1,
-		currentUrl: '',
-		categoryUrls:  {},
-	}
+    session.attributes = {
+        currentCategory: '',
+        currentOrder: 1,
+        currentUrl: '',
+        categoryUrls:  {},
+    }
 } 
 
 
